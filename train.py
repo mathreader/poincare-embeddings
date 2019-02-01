3a1ac9f3b1562aa618b266441d981e04c8d06d64
Max Jiang <qj46@cornell.edu>
2019-02-01 12:37:49 -0500

    alternate fix

--- train.py
+++ train.py
@@ -50,8 +50,10 @@ def train(model, data, optimizer, opt, log, rank=1, queue=None):
             loss = model.loss(preds, targets, size_average=True)
             loss.backward()
             optimizer.step(lr=lr)
-            #epoch_loss.append(loss.data[0])
-            epoch_loss.append(loss.item())
+            try:
+                epoch_loss.append(loss.data[0])
+            except:
+                epoch_loss.append(loss.item())
         if rank == 1:
             emb = None
             if epoch == (opt.epochs - 1) or epoch % opt.eval_each == (opt.eval_each - 1):
